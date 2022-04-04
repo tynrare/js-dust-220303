ce94be70bb1bd8d5cf17599ee8090db8971e375a
tynrare <tynrare@gmail.com>
2022-04-04 17:16:59 +0300

    raymarch cube added

---
+++ src/dust-220311/iquilezles-raymarch.glsl
@@ -0,0 +1,108 @@
+#define AA 3
+
+float sdCylinder(vec3 p, vec3 a, vec3 b, float r)
+{
+    vec3  ba = b - a;
+    vec3  pa = p - a;
+    float baba = dot(ba,ba);
+    float paba = dot(pa,ba);
+    float x = length(pa*baba-ba*paba) - r*baba;
+    float y = abs(paba-baba*0.5)-baba*0.5;
+    float x2 = x*x;
+    float y2 = y*y*baba;
+    
+    float d = (max(x,y)<0.0)?-min(x2,y2):(((x>0.0)?x2:0.0)+((y>0.0)?y2:0.0));
+    
+    return sign(d)*sqrt(abs(d))/baba;
+}
+
+float sdRoundBox( vec3 p, vec3 b, float r )
+{
+  vec3 q = abs(p) - b;
+  return length(max(q,0.0)) + min(max(q.x,max(q.y,q.z)),0.0) - r;
+}
+
+float map( in vec3 pos )
+{
+    return sdRoundBox(pos, vec3(0.1, 0.1, 0.1), 0.07 );
+	//return sdCylinder(pos, vec3(-0.2,-0.3,-0.1), vec3(0.3,0.3,0.4), 0.2 );
+}
+
+// http://iquilezles.org/www/articles/normalsSDF/normalsSDF.htm
+vec3 calcNormal( in vec3 pos )
+{
+    vec2 e = vec2(1.0,-1.0)*0.5773;
+    const float eps = 0.0005;
+    return normalize( e.xyy*map( pos + e.xyy*eps ) + 
+					  e.yyx*map( pos + e.yyx*eps ) + 
+					  e.yxy*map( pos + e.yxy*eps ) + 
+					  e.xxx*map( pos + e.xxx*eps ) );
+}
+   
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord, in vec2 resolution, in float time )
+{
+     // camera movement	
+	float an = 0.5*(time-10.0);
+	vec3 ro = vec3( 1.0*cos(an), 0.707, 1.0*sin(an) );
+    vec3 ta = vec3( 0.0, 0.0, 0.0 );
+    // camera matrix
+    vec3 ww = normalize( ta - ro );
+    vec3 uu = normalize( cross(ww,vec3(0.0,1.0,0.0) ) );
+    vec3 vv = normalize( cross(uu,ww));
+
+        
+    vec3 tot = vec3(0.0);
+    
+    #if AA>1
+    for( int m=0; m<AA; m++ )
+    for( int n=0; n<AA; n++ )
+    {
+        // pixel coordinates
+        vec2 o = vec2(float(m),float(n)) / float(AA) - 0.5;
+        vec2 p = (-resolution.xy + 2.0*(fragCoord+o))/resolution.y;
+        #else    
+        vec2 p = (-resolution.xy + 2.0*fragCoord)/resolution.y;
+        #endif
+
+	    // create view ray
+        vec3 rd = normalize( p.x*uu + p.y*vv + 1.5*ww );
+
+        // raymarch
+        const float tmax = 3.0;
+        float t = 0.0;
+        for( int i=0; i<256; i++ )
+        {
+            vec3 pos = ro + t*rd;
+            float h = map(pos);
+            if( h<0.0001 || t>tmax ) break;
+            t += h;
+        }
+        
+    
+       // shading/lighting	
+        vec3 col = vec3(0.569, 0.984, 0.678);
+        if( t<tmax )
+        {
+			/*
+            vec3 pos = ro + t*rd;
+            vec3 nor = calcNormal(pos);
+            float dif = clamp( dot(nor,vec3(0.57703)), 0.0, 1.0 );
+            float amb = 0.5 + 0.5*dot(nor,vec3(0.0,1.0,0.0));
+            col = vec3(0.2,0.3,0.4)*amb + vec3(0.8,0.7,0.5)*dif;
+			*/
+			col = vec3(0.973, 0.196, 0.392);
+        }
+
+        // gamma        
+        //col = sqrt( col );
+	    tot += col;
+    #if AA>1
+    }
+    tot /= float(AA*AA);
+    #endif
+
+	fragColor = vec4( tot, 1.0 );
+}
+
+#pragma glslify: export(mainImage)
--- src/dust-220311/shader.frag
+++ src/dust-220311/shader.frag
@@ -4,17 +4,10 @@ uniform vec2 resolution;
 uniform vec3 pointer;
 
 #pragma glslify: circleImage = require('./iquilezles-circle.glsl')
+#pragma glslify: raymarch = require('./iquilezles-raymarch.glsl')
 
 
 void main() {
-  circleImage(gl_FragColor, gl_FragCoord.xy, resolution, pointer);
-  /*
-  float vmin = min(resolution.y, resolution.x);
-  vec2 p = (gl_FragCoord.xy - .5 * resolution) / vmin;
-  float r = .5 + .5 * sin(5. * log(length(p)) - time * 1.2);
-  float g = .5 + .5 * sin(5. * log(length(p)) + sin(time + 2. * p.x));  
-  float b = .5 + .5 * sin(.2 + 5. * log(length(p)) + sin(time * .4 + 4. * p.y));
-  gl_FragColor = vec4(r, g, b, 1.);
-  */
-
+  //circleImage(gl_FragColor, gl_FragCoord.xy, resolution, pointer);
+  raymarch(gl_FragColor, gl_FragCoord.xy, resolution, time);
 }
